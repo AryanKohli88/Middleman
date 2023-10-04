e9a8d03247bc6dcbddcee173649d88c09567d781
Aryan Kohli <aryankohli0805@gmail.com>
2023-10-04 23:34:36 +0530

    ERROR@server.js:109 it seems

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /*
+
 EMPTY FILE
 
 // var express = require('express')
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// authURL -> has authorization code. (http://localhost:3000/google/callback?code=4%2F0AfJohXnV5VGq3EytlM5-DG-OXFgFwibilUK5oFfHDkKPTjPxfKxJWlVmSJ-vvwR-eCnYcg&scope=profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile)
+// authURL -> has authorization code. ( for eg. http://localhost:3000/google/callback?code=4%2F0AfJohXnV5VGq3EytlM5-DG-OXFgFwibilUK5oFfHDkKPTjPxfKxJWlVmSJ-vvwR-eCnYcg&scope=profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile)
 // auth code is exchanged for access token and refresh token using oauth2client object.
 // this access token will be used to make request to YT API.
 
@@ -106,6 +106,8 @@ const upload = multer({
 }).single('file');
 
 app.post('/upload', async (req, res) => {
+  // console.log(res) // PROBLEM
+  // console.log(res.query) // is empty
   const code = req.query.code;
   const { tokens } = await oauth2Client.getToken(code);
   // const channelName = await getChannelName(tokens.access_token)
@@ -147,7 +149,7 @@ app.post('/upload', async (req, res) => {
         tags: tags,
       },
       status: {
-        privacyStatus: 'private', // You can set this to 'public' if needed
+        privacyStatus: 'private', 
       },
     };
 
@@ -172,119 +174,3 @@ app.post('/upload', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-/*
-app.post('/upload', async (req,res) => {
-  
-  // const { tokens } = await oauth2Client.getToken(code);
-  
-  upload(req,res,function(err){
-    if(err){
-      console.error('Error while exchanging the authorization code for tokens:', err);
-    }
-    console.log(req.file.path)
-    title = 'video title'
-    description = 'video'
-    tags =  ['my-tag1', 'my-tag2']
-
-    const youtube = google.youtube({
-      version: 'v3',
-      auth:oauth2Client
-    })
-
-    youtube.videos.insert(
-    {
-      resource:{
-        snippet: {
-                // categoryId: "22",
-                title: 'My Video Title',
-                description: 'My Video Description',
-                tags: ['my-tag1', 'my-tag2'],
-              },
-        status: {
-                privacyStatus: 'private',
-              },
-      },
-      part:"snippet,status",
-      media:{
-        body:fs.createReadStream(req.file.path)
-        }
-    },
-
-      (err,data) => {
-        if(err){
-          console.error(err);
-          return res.status(500).send('Error uploading the file.');
-        }
-        console.log("upload complete")
-
-        res.render("Success") //, {name,name,pic:pic,success:true})
-      }
-    )
-    
-  })
-})
-*/
-
-
-
-/*
- app.post('/upload', async (req, res) => {
-
-// Ensure the request is authenticated and contains an access token
-//   const accessToken = req.query.tokens.access_token;
-//   if (!accessToken) {
-//     res.status(401).send('Authentication is required to upload videos.');
-//     return;
-//   }
-
-//   // Check if a video file is included in the request
-//   if (!req.files || !req.files.videoFile) {
-//     res.status(400).send('Please select a video file for upload.');
-//     return;
-//   }
-
-//   // Upload the video to YouTube using the YouTube Data API
-//   const videoFile = req.files.videoFile;
-//   const videoMetadata = {
-//     snippet: {
-//       title: 'Your Video Title',
-//       description: 'Your video description',
-//       tags: ['tag1', 'tag2'],
-//     },
-//   };
-
-  try {
-    // Initialize the YouTube API client
-    // const youtube = google.youtube({
-    //   version: 'v3',
-    //   auth: accessToken,
-    // });
-
-    // // Upload the video
-    // const uploadResponse = await youtube.videos.insert({
-    //   part: 'snippet,status',
-    //   media: {
-    //     mimeType: 'video/*',
-    //     body: fs.createReadStream(videoFile.path),
-    //   },
-    //   resource: videoMetadata,
-    // });
-
-    console.log("\nRequest is :: \n");
-    console.log(req); // returns empty array, => request nhi milra kuchh api ko => error at line 90.
-    console.log("\nResponse is :: \n");
-    console.log(res.data); // res.data is undefined//
-
-
-    
-    res.send('Video uploaded successfully! Video ID: ');// + uploadResponse.data.id);
-  } catch (error) {
-    console.error('Error while uploading video:', error);
-    res.status(500).send('Video upload failed. Please try again.');
-  }
-});
-
-*/
